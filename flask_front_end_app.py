e148a6487d7edb3b8122b4f48cffc3d5862fb391
liamneath1 <liamhneath@gmail.com>
2016-05-27 11:48:21 -0700

    TEST

--- flask_front_end_app.py
+++ flask_front_end_app.py
@@ -1,9 +1,9 @@
 import os, copy
 from flask import Flask, jsonify, request, send_from_directory, make_response
-from flask.ext.sqlalchemy import SQLAlchemy, create_engine
+from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.heroku import Heroku
 
-# from sqlalchemy import create_engine
+ from sqlalchemy import create_engine
 # from sqlalchemy.ext.declarative import declarative_base
 
 
