383e59c32ab2e4dd625f299ea54ed3dd0e96b6ac
liamneath1 <liamhneath@gmail.com>
2016-05-27 14:42:46 -0700

    HELLO

--- app/assets/js/data-fetch.js
+++ app/assets/js/data-fetch.js
@@ -305,8 +305,8 @@ function processData(){
 $("#ticketCode").keypress(function(e) {
     var curr = document.getElementById("ticketCode").value
     console.log(curr + String.fromCharCode(e.which) )
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', "https://arcane-springs-65260.herokuapp.com/testQuery", true);
+    var xhr = new XMLHttpsRequest();
+    xhr.open('GET', "//arcane-springs-65260.herokuapp.com/testQuery", true);
     xhr.send();
     console.log(xhr.responseText);
 });
