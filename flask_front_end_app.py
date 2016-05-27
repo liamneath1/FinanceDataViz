3c8ec38d6e7e3348223a26aeb1857c39ea7f6f80
liamneath1 <liamhneath@gmail.com>
2016-05-27 12:04:44 -0700

    YO

--- flask_front_end_app.py
+++ flask_front_end_app.py
@@ -11,14 +11,12 @@ from sqlalchemy.ext.declarative import declarative_base
 app = Flask(__name__, static_url_path='')
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 engine = create_engine('postgres://jwnrvwczsmwahh:Eay8klzAFN5V0xljHial-krhxv@ec2-174-129-242-241.compute-1.amazonaws.com:5432/d4v4mh5fc6v6hu')
+
 Base = declarative_base()
 Base.metadata.reflect(engine)
-
-
 from sqlalchemy.orm import relationship, backref
-
-class Users(Base):
-    __table__ = Base.metadata.tables['stockInfo']
+# class Users(Base):
+#     __table__ = Base.metadata.tables['stockInfo']
 
 
 
