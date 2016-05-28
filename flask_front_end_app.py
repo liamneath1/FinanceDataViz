d56f4f3fbbeda1fdcb0decad788c4b0dce0275aa
liamneath1 <liamhneath@gmail.com>
2016-05-27 18:34:45 -0700

    YO

--- flask_front_end_app.py
+++ flask_front_end_app.py
@@ -44,7 +44,7 @@ def testQuery():
 	# resp = Response(response = data, status = 200, mimetype = "application/json")
 	# return (resp)
 	#return json.dumps([dict(r) for r in res]), 200, {'Content-Type': 'application/json'}
-	return (jsonify([dict(r) for r in res]))
+	return (jsonify(json.dumps([dict(r) for r in res]))
 
 # send assets (ex. assets/js/random_triangle_meshes/random_triangle_meshes.js)
 # blocks other requests, so your directories won't get listed (ex. assets/js will return "not found")
