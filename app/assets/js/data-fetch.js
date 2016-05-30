fc84081bf39d35d4748a6900a01aa4f75e995c20
Jessica <jessica.zhang96@gmail.com>
2016-05-29 20:36:48 -0700

    fixing charts

--- app/assets/js/data-fetch.js
+++ app/assets/js/data-fetch.js
@@ -192,7 +192,8 @@ function loadCompany(method){
         $('#quarter-chart').empty();
         $('#fluctuation-chart').empty();
         $('#closing-price-chart').empty();
-        
+
+        d3.selectAll("svg").remove()
         fluctuation.filterRange([-50000,50000]);
 
         cf.remove();
@@ -212,48 +213,9 @@ function loadCompany(method){
 
 function fetchAndAdd(chartReference){
     if (chartReference === 'A'){
-        fluctuationChart = dc.barChart('#fluctuation-chart');
 
-        fluctuationChart
-                .width(420)
-                .height(180)
-                .margins({top: 10, right: 50, bottom: 30, left: 40})
-                .dimension(fluctuation)
-                .group(fluctuationGroup)
-                .elasticY(true)
-                .centerBar(true)
-                .gap(1)
-                .round(dc.round.floor)
-                .x(d3.scale.linear().domain([-25,25]))
-                .renderHorizontalGridLines(true);
-            
-            fluctuationChart.xAxis().tickFormat(
-                function (v) { return v + '%'; });
-            fluctuationChart.yAxis().ticks(10);  
-            var volumeByDate = cf.dimension(function(d){
-               return (d.dd); 
-            });
-            
     }else if (chartReference === 'B'){
-        console.log("b");
-        fluctuationChart.resetSvg();
-        volumeByDate = cf.dimension(function(d){
-               return (d.dd); 
-            });
-        fluctuationChart
-                .width(420)
-                .height(180)
-                .margins({top: 10, right: 50, bottom: 30, left: 40})
-                .dimension(volumeByDate)
-                .group(volumeByDateGroup)
-                .elasticY(true)
-                .centerBar(true)
-                .gap(1)
-                .round(dc.round.floor)
-                .x(d3.scale.linear().domain([-25,25]))
-                .renderHorizontalGridLines(true);
-        dc.renderAll();
-        dc.redrawAll();
+        
     }else if (chartReference === 'C'){
 
     }else if (chartReference === 'D'){
