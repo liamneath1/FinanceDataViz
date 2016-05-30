60ea6360b26d8b4900eb5629f60ea266593b2be4
liamneath1 <liamhneath@gmail.com>
2016-05-29 18:13:04 -0700

    Hello

--- flask_front_end_app.py
+++ flask_front_end_app.py
@@ -47,7 +47,7 @@ def upload_logo():
 	file = request.files['file']
 	if file and allowed_file(file.filename):
 		filename = secure_filename(file.filename)
-		file.save(os.path.join(app.config['UPLOAD_FOLDER'],filename))
+		file.save(os.path.join(app.config['UPLOAD_FOLDER']))
 		return redirect(url_for('uploaded_file',filename=filename))
 
 
