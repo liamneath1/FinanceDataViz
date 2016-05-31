5bf234fc0da3a460305a123b3099b02031968a11
Jessica <jessica.zhang96@gmail.com>
2016-05-31 10:39:13 -0700

    adding high-low chart

--- app/assets/js/data-fetch.js
+++ app/assets/js/data-fetch.js
@@ -220,6 +220,7 @@ function loadCompany(method){
 }
 
 function compareCompany(){
+        d3.selectAll("svg").remove();
         var settings;
         var ticketCode;
         if(document.getElementById('ticketCode').value ===''){
@@ -254,6 +255,16 @@ function compareCompany(){
         $.ajax(settings).done(function (response) {
             ticketCode = response[0].tickername;
             ticketCode = ticketCode.replace(/\s/g, '');
+            
+            $('#gain-loss-chart').empty();
+            $('#quarter-chart').empty();
+            $('#fluctuation-chart').empty();
+            $('#closing-price-chart').empty();
+            
+            fluctuation.filterRange([-50000,50000]);
+            cf.remove();
+            dc.renderAll();
+            dc.redrawAll();
 
             request = "https://www.quandl.com/api/v3/datasets/WIKI/"+ticketCode +"/data.csv?api_key=1Y3h3-Q8VW1Z1tZXqhpH";
             numOverlap = 1;
